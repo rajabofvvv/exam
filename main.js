0147bfc4f62e37420950f0b70ae174466b2493d0
rajabov umid <91966707+relaxel@users.noreply.github.com>
2023-01-07 12:24:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// 2
+// let a = 76
+// let b = 6
+// let c = 7
+// 
+// 
+// console.log(Math.max(a,b,c));
+// console.log(Math.min(a,b,c));
+
+// 3
+// let a =1200
+// 
+// let b = a%1000/100
+// b  
+
+// 4
+// let a = 8
+// let b = 6
+// let c = 7
+// 
+// if((a-b)<(a-c)){
+    // console.log('Yaqin nuqta:', b);
+    // console.log('Ular orasdagi masofa: ',a-b);
+// } else if((a-c)<(a-b)){
+    // console.log('Yaqin nuqta:', c);
+    // console.log('Ular orasdagi masofa: ',a-c);
+// }
+
+// 5
+// let a = 8
+// 
+// if((a>0)&&(a%2==0)){
+    // console.log('Musbat juft son',a);
+// }else{
+    // console.log('Musbat toq son',a);
+// }
+
+// 6
+
+let a =10
+let n = 5
+
+for(i=1;i<=n;i++){
+    rez = rez*a
+    console.log(rez);
+}
